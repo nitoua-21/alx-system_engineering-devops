02fd6ea777e053e8a98fcb57aeda7f84aaed99e2
Normich ITOUA <nitoua.dev@gmail.com>
2024-06-29 00:14:35 +0100

    Update task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Using Puppet, install puppet-lint
 
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
